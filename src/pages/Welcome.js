ff9e001529cd20de06910fad20055f9d301ce1fd
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-07 12:46:19 +0100

    274. Working with Nested Routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 const Welcome = () => {
   return (
-    <h1>Welcome</h1>
+    <section>
+
+      <h1>Welcome</h1>
+      <Route path='/welcome/new-user'>
+        <p>Welcome, new user!</p>
+      </Route>
+    </section>
   )
 }
 
