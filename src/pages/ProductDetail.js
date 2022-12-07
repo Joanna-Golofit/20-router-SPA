4855dee99858010562f555ecc2271eb7498edc86
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-07 12:23:07 +0100

    272. Extracting Route Params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 
 const ProductDetail = () => {
+  const params = useParams();
+  console.log(params)
   return (
     <section>
       <h1>Product Detail</h1>
+      <h1>product Id: {params.productId}</h1>
     </section>
   )
 }
